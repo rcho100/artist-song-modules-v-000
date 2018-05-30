f674d32f4e72f8fdb889b89b8f692e854f377634
Richard Cho <richardcho100@gmail.com>
2018-05-30 21:38:41 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 class Artist
   extend Memorable
-   
+
   attr_accessor :name
   attr_reader :songs
 
@@ -20,14 +20,6 @@ class Artist
   def self.all
     @@artists
   end
-
-  # def self.reset_all
-  #   self.all.clear
-  # end
-  #
-  # def self.count
-  #   @@artists.count
-  # end
 
   def add_song(song)
     @songs << song
